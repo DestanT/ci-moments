b5470f834008d6bfc5f86f8f5fea93530f86a476
Destan Tezel <destantezel@googlemail.com>
2024-01-09 14:48:57 +0000

    Add routes to signup and '404 - page not found'

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
-import { Route } from 'react-router-dom/cjs/react-router-dom.min';
 import styles from './App.module.css';
 import NavBar from './components/NavBar';
 import Container from 'react-bootstrap/Container';
-import { Router, Switch } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div className={styles.App}>
-      <NavBar/>
+      <NavBar />
       <Container className={styles.Main}>
         <Switch>
           <Route exact path='/' render={() => <h1>Home Page</h1>} />
           <Route exact path='/signin' render={() => <h1>Sign In</h1>} />
+          <Route exact path='/signup' render={() => <h1>Sign Up</h1>} />
+          <Route render={() => <p>Page not found!</p>} />
         </Switch>
       </Container>
     </div>
