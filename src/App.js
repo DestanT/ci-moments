2b8bb31288b3c294b1badbeffe4416257887f6d1
Destan Tezel <destantezel@googlemail.com>
2024-01-09 14:19:13 +0000

    Import BrowserRouter, Router and Switch and create homepage and signin routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
+import { Route } from 'react-router-dom/cjs/react-router-dom.min';
 import styles from './App.module.css';
 import NavBar from './components/NavBar';
-import Container from "react-bootstrap/Container";
+import Container from 'react-bootstrap/Container';
+import { Router, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div className={styles.App}>
       <NavBar/>
       <Container className={styles.Main}>
-        <h1>Home Page</h1>
-        <h1>Sign In</h1>
+        <Switch>
+          <Route exact path='/' render={() => <h1>Home Page</h1>} />
+          <Route exact path='/signin' render={() => <h1>Sign In</h1>} />
+        </Switch>
       </Container>
     </div>
   );
